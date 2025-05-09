19c64053e260ad3b199e77f52e525f10316710db
Macintosh <realdave9t9@gmail.com>
2025-05-10 00:10:21 +0100

    Update homepage layout and fix button bug

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/index" element={<Index />} />
+          <Route path="" element={<Index />} />
         </Routes>
       </Router>
     </>
